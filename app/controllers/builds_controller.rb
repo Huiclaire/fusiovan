082d4a3b7200ccfc58c9fe56c89c31620eb4ca2f
Claire Lin <chunhuilin7516@gmail.com>
2022-06-09 12:54:05 +0800

    showpage html

--- app/controllers/builds_controller.rb
+++ app/controllers/builds_controller.rb
@@ -2,4 +2,8 @@ class BuildsController < ApplicationController
   def index
     @builds = Build.includes(:name, :total_price, :solar_panel_id, :battery_id, :user_id)
   end
+
+  def show
+    @builds = Build.find(params[:id])
+  end
 end
