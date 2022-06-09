94fce307fc83859669ee5187c9f57af6a7d5b443
Tim Banta <timbanta@me.com>
2022-06-09 17:25:39 +0800

    added pages and links for appliances and components pages

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   get 'dashboard', to: 'profiles#dashboard'
   get 'comparison', to: 'profiles#comparison'
+  resources :appliances, only: :index
+  resources :components, only: :index
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :builds, only: [ :index, :show ]
 end
