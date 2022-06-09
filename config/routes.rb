65724f465cf4acebdde06b229eede96ed8a73a4c
Tim Banta <timbanta@me.com>
2022-06-09 17:18:07 +0800

    added dashboard and comparison pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  get 'dashboard', to: 'profiles#dashboard'
+  get 'comparison', to: 'profiles#comparison'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :builds, only: [ :index, :show ]
 end
